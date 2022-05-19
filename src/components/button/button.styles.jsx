dfac74ef0b5e96d98f584b19c07d6dec6d94521d
Luciano Muniz <luciano.mo@gmail.com>
2022-05-19 08:13:07 -0300

    added netlify serverless payment function as well as stripe library config

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
 
 export const BaseButton = styled.button`
-  min-width: 165px;
-  width: auto;
-  height: 50px;
-  letter-spacing: 0.5px;
-  line-height: 50px;
-  padding: 0 35px 0 35px;
-  font-size: 15px;
+  align-items: center;
   background-color: black;
-  color: white;
-  text-transform: uppercase;
-  font-family: 'Open Sans Condensed';
-  font-weight: bolder;
   border: none;
+  color: white;
   cursor: pointer;
   display: flex;
+  font-family: 'Open Sans Condensed';
+  font-size: 15px;
+  font-weight: bolder;
+  height: 50px;
   justify-content: center;
-
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  min-width: 165px;
+  padding: 0 35px 0 35px;
+  text-transform: uppercase;
+  width: auto;
   &:hover {
     background-color: white;
     color: black;
@@ -28,7 +28,6 @@ export const BaseButton = styled.button`
 export const GoogleSignInButton = styled(BaseButton)`
   background-color: #4285f4;
   color: white;
-
   &:hover {
     background-color: #357ae8;
     border: none;
@@ -37,12 +36,32 @@ export const GoogleSignInButton = styled(BaseButton)`
 
 export const InvertedButton = styled(BaseButton)`
   background-color: white;
-  color: black;
   border: 1px solid black;
-
+  color: black;
   &:hover {
     background-color: black;
-    color: white;
     border: none;
+    color: white;
+  }
+`;
+
+export const LoadingSpinner = styled.div`
+  animation: spin 1s ease-in-out infinite;
+  display: inline-block;
+  border: 3px solid rgba(195, 195, 195, 0.6);
+  border-radius: 50%;
+  border-top-color: #636767;
+  height: 30px;
+  width: 30px;
+  -webkit-animation: spin 1s ease-in-out infinite;
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
   }
 `;
